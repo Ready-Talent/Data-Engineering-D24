ff17c40fde317a6acf2672a4b3aa82463e014043
lojainwail01 <lojainph@gmail.com>
2024-04-20 16:37:01 +0200

    lojain gcs to bq v.1

---
+++ dags/lojain_gcstobq.py
@@ -0,0 +1,41 @@
+# implement Hello World DAG
+
+import logging
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
+
+
+
+dag = DAG(
+    dag_id="hello_lojain",
+    description="Simple tutorial DAG",
+    schedule_interval=None,
+    start_date=datetime(2021, 1, 1),
+    catchup=False,
+)
+
+
+start_task = EmptyOperator(task_id="start", dag=dag)
+DATASET_NAME="lojain_fromgcs"
+TABLE_NAME="taxi_info"
+load_csv = GCSToBigQueryOperator(
+    task_id="lojain_gcstobq",
+    bucket="chicago-taxi-test-de24",
+    source_objects=["chicago-taxi-test-de24/*.csv"],
+    destination_project_dataset_table=f"{DATASET_NAME}.{TABLE_NAME}",
+    schema_fields=[
+        {"name": "name", "type": "STRING", "mode": "NULLABLE"},
+        {"name": "post_abbr", "type": "STRING", "mode": "NULLABLE"},
+    ],
+    create_disposition='CREATE_IF_NEEDED',
+    write_disposition="WRITE_TRUNCATE",
+    schema= True
+)
+
+end_task = EmptyOperator(task_id="end", dag=dag)
+
+start_task >> load_csv >> end_task
+
