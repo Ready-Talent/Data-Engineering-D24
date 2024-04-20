cdceb34be207691614ead9513431f29612273753
Abdelrahman Tamer <beedoz2000@gmail.com>
2024-04-20 15:05:40 +0200

    print hello dag implementation

---
+++ dags/abdelrahman-tamer/my_first_dag.py
@@ -0,0 +1,26 @@
+# implement Hello World DAG
+
+import logging
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
+
+
+def print_hello():
+    print("hello world")
+    
+
+dag = DAG(dag_id="my_first_dag", schedule_interval=None, start_date=datetime(2021, 1, 1), catchup=False)
+
+
+task1 = EmptyOperator(task_id="start_task", dag=dag)
+task2 = PythonOperator(task_id="print_hello_task", python_callable=print_hello, dag=dag)
+task3 = EmptyOperator(task_id="end_task", dag=dag)
+
+
+
+
+
+
