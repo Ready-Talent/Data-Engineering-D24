a981bbfcfc2912c811e570323e1371f69fe072b7
Abdelrahman Tamer <beedoz2000@gmail.com>
2024-04-20 16:44:42 +0200

    chicago taxi dag

---
+++ dags/abdelrahman-tamer/gcs_to_bq.py
@@ -0,0 +1,29 @@
+from airflow import DAG
+import datetime
+from airflow.operators.empty import EmptyOperator
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+
+
+dag = DAG (
+        dag_id="abdelrahman_06_gcs_to_bq", 
+        schedule_interval=None, 
+        start_date=datetime(2021, 1, 1), catchup=False
+    )
+
+start = EmptyOperator(task_id="start_task", dag=dag)
+
+load_data = GCSToBigQueryOperator(
+    task_id = "chicago_taxi_gcs_to_bq",
+    bucket="chicago-taxi-test-de24",
+    source_objects="data/*",
+    source_format="CSV",
+    destination_project_dataset_table="SRC_06.chicago_taxi",
+    autodetect=True,
+    write_disposition="WRITE_TRUNCATE",
+    create_disposition="CREATE_IF_NEEDED",
+    dag=dag
+    )
+
+end = EmptyOperator(task_id="end_task", dag=dag)
+
+start >> load_data >> end
