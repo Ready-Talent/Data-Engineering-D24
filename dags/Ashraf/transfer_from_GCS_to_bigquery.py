be4e9b8efc1bde7a9418e8827aa9e2242a264e8c
Mohamed Ashraf <93071951+TmohamedashrafT@users.noreply.github.com>
2024-04-20 08:06:35 -0700

    Update transfer_from_GCS_to_bigquery.py

--- dags/Ashraf/transfer_from_GCS_to_bigquery.py
+++ dags/Ashraf/transfer_from_GCS_to_bigquery.py
@@ -24,7 +24,6 @@ load_csv = GCSToBigQueryOperator(
     destination_project_dataset_table="chicago_taxi_01.test_table",
     dag=dag,
     field_delimiter=';',
-    allow_jagged_rows = True
 )
 end_task = EmptyOperator(task_id="end_task", dag=dag)
 
