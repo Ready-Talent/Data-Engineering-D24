03e076d9d48f17824ae06ce637e57f8a6fa94a5b
MohamedGalal2 <modygalal2014@gmail.com>
2024-04-23 20:59:49 +0200

    Load

--- dags/Galal_Dag/postgres2bq.py
+++ dags/Galal_Dag/postgres2bq.py
@@ -21,34 +21,31 @@ dag = DAG(
     catchup=False,
 )
 
-DATASET_NAME="Galal_Airflow"
-TABLE_NAME="chicago-taxi-test-de24"
+DATASET_NAME="Galal_Airflow_pg2bq"
+TABLE_NAME="order"
 bucket="postgres-to-gcs",
 
 start_task = EmptyOperator(task_id="start_task", dag=dag)
 get_data = PostgresToGCSOperator(
     task_id="postgres_to_gcs_example_Galal",
     postgres_conn_id="Galal_Pg_Connection",
-    sql="SELECT * from src09.product",
+    sql="SELECT * from src01.product",
     bucket=bucket,
-    filename="Galal/",
+    filename="Galal/order.csv",
     gzip=False,
-    export_format = 'CSV',
+    export_format = 'CSV'
 )
 
 load_csv = GCSToBigQueryOperator(
-    task_id="gcs_to_bigquery_example_Galal",
-    bucket="chicago-taxi-test-de24",
-    source_objects=["data/*.csv"],
+    task_id="gcs_to_bigquery_example_Galal2",
+    bucket=bucket,
+    source_objects=["Galal/*.csv"],
     destination_project_dataset_table=f"{DATASET_NAME}.{TABLE_NAME}",create_disposition='CREATE_IF_NEEDED',
     write_disposition="WRITE_TRUNCATE",
-    field_delimiter=';', 
-    skip_leading_rows =1, # skip Headerss 
-    ignore_unknown_values=True,
-    max_bad_records=100000
+    skip_leading_rows =1# skip Headerss 
 
 )
 
 end_task = EmptyOperator(task_id="end_task", dag=dag)
 
-start_task >> get_data >> end_task
+start_task >> get_data >>load_csv >>end_task
