f2b9d1322e729bd359791fd42fdc77d9861431a2
ReemAbdulraouf <76842378+ReemAbdulraouf@users.noreply.github.com>
2024-04-23 20:39:14 +0200

    Move data from postgres to gcs

---
+++ dags/Reem/postgres_to_gcs.py
@@ -0,0 +1,36 @@
+import logging
+from datetime import datetime
+from webbrowser import get
+
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.providers.google.cloud.transfers.postgres_to_gcs import PostgresToGCSOperator
+
+
+def move_data():
+    logging.info("Move data from postgres to gcs")
+    return "printed"
+
+
+dag = DAG(
+    dag_id="move_from_postgres_to_gcs_reemaa",
+    description="postgres_to gcs DAG",
+    schedule_interval=None,
+    start_date=datetime(2021, 1, 1),
+    catchup=False,
+)
+
+start_task = EmptyOperator(task_id="start_task", dag=dag)
+
+get_data = PostgresToGCSOperator(
+    task_id="get_data_reema",
+    postgres_conn_id="postgres_conn_reema",
+    sql="SELECT * from src01.order",
+    bucket="postgres-to-gcs",
+    filename="Reemaa/",
+    gzip=False,
+)
+
+end_task = EmptyOperator(task_id="end_task", dag=dag)
+
+start_task >> get_data >> end_task
