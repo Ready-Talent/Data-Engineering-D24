758180be60e99d7c8b57c5234b4800f64a1ab005
Abdelrahman Tamer <beedoz2000@gmail.com>
2024-04-23 20:31:48 +0200

    dag for moving postgres data to bigquery

---
+++ dags/abdelrahman-tamer/moving_postgres_to_bq.py
@@ -0,0 +1,46 @@
+from airflow import DAG
+from datetime import datetime
+from airflow.operators.empty import EmptyOperator
+from airflow.providers.google.cloud.transfers.postgres_to_gcs import PostgresToGCSOperator
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+
+gcs_bucket = "postgres-to-gcs"
+
+dag  = DAG(
+        dag_id="abdelrahman_06_postgres_bq_dag", 
+        schedule_interval=None, 
+        start_date=datetime(2021, 1, 1), 
+        catchup=False,
+    )
+
+
+start_task = EmptyOperator(task_id="start_task", dag=dag)
+
+pg_to_gcs = PostgresToGCSOperator(
+        task_id="pg_to_gcs",
+        postgres_conn_id="abdelrahman_06_postgres_connection",
+        bucket=gcs_bucket,
+        filename="abdelrahman_06/address.csv",
+        sql="SELECT * FROM src01.address;",
+        dag=dag
+    )
+
+move_data_to_bigquery_table = GCSToBigQueryOperator(
+        task_id = "move_data_to_bigquery_table",
+        bucket=gcs_bucket,
+        source_objects="abdelrahman_06/address.csv",
+        source_format="CSV",
+        destination_project_dataset_table="SRC_06.address",
+        autodetect=True,
+        field_delimiter=';',
+        max_bad_records=1000000,
+        skip_leading_rows=1,
+        write_disposition="WRITE_TRUNCATE",
+        create_disposition="CREATE_IF_NEEDED",
+        encoding='UTF-8',
+        dag=dag
+    )
+
+end_task = EmptyOperator(task_id="end_task", dag=dag)
+
+start_task >> pg_to_gcs >>move_data_to_bigquery_table >> end_task
