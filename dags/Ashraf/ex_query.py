ce4e4c0dd0314a88816930833b279d7b930d33c2
Mohamed Ashraf <93071951+TmohamedashrafT@users.noreply.github.com>
2024-04-23 13:02:47 -0700

    Update ex_query.py

--- dags/Ashraf/ex_query.py
+++ dags/Ashraf/ex_query.py
@@ -20,7 +20,8 @@ start_task = EmptyOperator(task_id="start_task", dag=dag)
 ecx_query = BigQueryExecuteQueryOperator(
     task_id="BigQuery_Execute_Query",
     sql= 'create_table.sql',
-    dag=dag
+    dag=dag,
+    use_legacy_sql=False
 )
 
 end_task = EmptyOperator(task_id="end_task", dag=dag)
