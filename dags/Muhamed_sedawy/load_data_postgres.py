ca3891e51e2da11e86afd43b681607d27a7a89c6
Sedawy001 <118743640+Sedawy001@users.noreply.github.com>
2024-04-25 11:32:29 +0200

    Update load_data_postgres.py

--- dags/Muhamed_sedawy/load_data_postgres.py
+++ dags/Muhamed_sedawy/load_data_postgres.py
@@ -6,33 +6,32 @@ from datetime import datetime, timedelta
 
 dag = DAG(
     dag_id="postgres_to_bigquery_sedawy",
-    description='A DAG to transfer data from PostgreSQL to Google Cloud Storage to BigQuery',
     schedule_interval=None,
     start_date=datetime(2021, 1, 1),
     catchup=False,
 )
 
-with dag:
     # Task to execute SQL command in PostgreSQL to extract data and save to GCS
-    extract_to_gcs_task = PostgresToGCSOperator(
-        task_id="get_data",
-        postgres_conn_id= 'sedawy_connections',
-        sql='select *  from src01.order',
-        bucket='postgres-to-gcs',
-        filename='Sedawy/order.csv',
+extract_to_gcs_task = PostgresToGCSOperator(
+    task_id="get_data",
+    postgres_conn_id= 'sedawy_connections',
+    sql='select *  from src01.order',
+    bucket='postgres-to-gcs',
+    filename='Sedawy/order.csv',
+    dag=dag
     )
 
     # Task to load data from GCS to BigQuery
-    load_to_bigquery_task = GCSToBigQueryOperator(
-        task_id='load_to_bigquery02',
-        bucket='postgres-to-gcs',
-        source_objects=["Sedawy/order.csv"],  # GCS source path
-        destination_project_dataset_table="AirFlow_02.order",  # BigQuery table to load data into
-        create_disposition='CREATE_IF_NEEDED',
-        skip_leading_rows=1,  # If your CSV has a header row
-        source_format='CSV',  # Source data format
-        dag=dag
+load_to_bigquery_task = GCSToBigQueryOperator(
+    task_id='load_to_bigquery02',
+    bucket='postgres-to-gcs',
+    source_objects=["Sedawy/order.csv"],  # GCS source path
+    destination_project_dataset_table="AirFlow_02.order",  # BigQuery table to load data into
+    create_disposition='CREATE_IF_NEEDED',
+    skip_leading_rows=1,  # If your CSV has a header row
+    source_format='CSV',  # Source data format
+    dag=dag
     )
 
     # Set task dependencies
-    extract_to_gcs_task >> load_to_bigquery_task
+extract_to_gcs_task >> load_to_bigquery_task
