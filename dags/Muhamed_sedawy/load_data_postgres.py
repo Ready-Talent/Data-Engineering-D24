f54b65707c8d63d6115ba94180620a242a83c16d
Sedawy001 <118743640+Sedawy001@users.noreply.github.com>
2024-04-25 08:30:16 +0200

    Update load_data_postgres.py

--- dags/Muhamed_sedawy/load_data_postgres.py
+++ dags/Muhamed_sedawy/load_data_postgres.py
@@ -26,7 +26,7 @@ with dag:
     load_to_bigquery_task = GCSToBigQueryOperator(
         task_id='load_to_bigquery02',
         bucket='postgres-to-gcs',
-        source_objects="Sedawy/order.csv",  # GCS source path
+        source_objects=['Sedawy/order.csv'],  # GCS source path
         destination_project_dataset_table="SRC_02.order",  # BigQuery table to load data into
         skip_leading_rows=1,  # If your CSV has a header row
         source_format='CSV',  # Source data format
