a428ec41499155a29fbbcc44501baae509f9c5f1
Sedawy001 <118743640+Sedawy001@users.noreply.github.com>
2024-04-25 13:35:35 +0200

    Update load_data_postgres.py

--- dags/Muhamed_sedawy/load_data_postgres.py
+++ dags/Muhamed_sedawy/load_data_postgres.py
@@ -2,7 +2,7 @@ from airflow import DAG
 from airflow.providers.google.cloud.transfers.postgres_to_gcs import PostgresToGCSOperator
 from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
 from datetime import datetime, timedelta
-
+bucket = 'postgres-to-gcs'
 
 dag = DAG(
     dag_id="postgres_to_bigquery_sedawy",
@@ -24,9 +24,9 @@ extract_to_gcs_task = PostgresToGCSOperator(
     # Task to load data from GCS to BigQuery
 load_to_bigquery_task = GCSToBigQueryOperator(
     task_id='load_to_bigquery02',
-    bucket='postgres-to-gcs',
-    source_objects=["Sedawy/order.csv"],  # GCS source path
-    destination_project_dataset_table='ready-data-engineering-p24.AirFlow_02.order',  # BigQuery table to load data into
+    bucket=bucket,
+    source_objects="Sedawy/order.csv",  # GCS source path
+    destination_project_dataset_table='AirFlow_02.order',  # BigQuery table to load data into
     create_disposition='CREATE_IF_NEEDED',
     skip_leading_rows=1,  # If your CSV has a header row
     source_format='CSV',  # Source data format
