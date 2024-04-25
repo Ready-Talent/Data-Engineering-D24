2b14f3e01c5af2d86c8f7eeb452855e651e05ae2
Mohamed Ashraf <93071951+TmohamedashrafT@users.noreply.github.com>
2024-04-25 18:25:33 +0200

    Update create_dim_customer.sql

--- dags/Ashraf/sql/create_dim_customer.sql
+++ dags/Ashraf/sql/create_dim_customer.sql
@@ -3,7 +3,7 @@ CREATE TABLE  Data_platform_01.dim_customer (
     customer_id INT64,
     customer_name STRING,
     email STRING,
-    phone INT64,
+    phone STRING,
     address_id INT64,
     address_street STRING,
     address_zipcode STRING,
