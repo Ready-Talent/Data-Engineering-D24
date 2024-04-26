e83b33dcdf7927dd5430c238e5a40327ac953394
ReemAbdulraouf <76842378+ReemAbdulraouf@users.noreply.github.com>
2024-04-26 16:27:52 +0300

    Create dim = customer

---
+++ dags/Reem/Sql/populate_dim_customer.sql
@@ -0,0 +1,12 @@
+INSERT INTO Data_Platform_Reema.dim_customer
+(customer_id, customer_name, email, phone , address_id, address_street, address_zipcode, city_id, city_name
+, state_id, state_name, country_id, country_name)
+
+select c.customer_id , c."name" customer_name, c.email , c.phone
+, a.address_id , a.street address_street, a.zipcode address_zipcode
+, -1 city_id, a.city city_name
+, -1 state_id, a.state state_name
+, -1 country_id, 'US' country_name
+from SRC_01.customer c
+left join SRC_04.address a
+on a.customer_id =c.customer_id;
