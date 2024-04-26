9517ae03c9dcb8715b87bfd2014b107e4f3f0e42
Abdelrahman Tamer <beedoz2000@gmail.com>
2024-04-27 00:49:41 +0300

    using BigQueryExecuteQueryOperator

--- dags/abdelrahman-tamer/bq_src_to_landing.py
+++ dags/abdelrahman-tamer/bq_src_to_landing.py
@@ -1,35 +1,25 @@
 from airflow import DAG
 from datetime import datetime
 from airflow.operators.empty import EmptyOperator
-from airflow.providers.google.cloud.operators.bigquery import BigQueryExecuteQueryOperator, BigQueryInsertJobOperator
-
-
-
+from airflow.providers.google.cloud.operators.bigquery import BigQueryExecuteQueryOperator
 
 dag  = DAG(
         dag_id="abdelrahman_06_bq_src_to_landing", 
         schedule_interval=None, 
         start_date=datetime(2021, 1, 1), 
-        catchup=False,
-        template_searchpath= "/dags/abdelrahman-tamer/sql"
+        catchup=False
     )
 
 
 start_task = EmptyOperator(task_id="start_task", dag=dag)
 
 
-create_table_and_insert_data = BigQueryInsertJobOperator(
-    task_id = "create_table_and_insert_data",
-    configuration={
-        "query": {
-            "query": "{% include 'dim_customer.sql' %}",
-            "'createDisposition": "CREATE_IF_NEEDED",
-            "writeDisposition": "WRITE_APPEND"
-        }
-        
-    },
+create_table_and_insert_data = BigQueryExecuteQueryOperator(
+    task_id="create_table_and_insert_data",
+    sql="sql/dim_customer.sql",
     dag=dag
 )
+
 
 end_task = EmptyOperator(task_id="end_task", dag=dag)
 
