8fe4d28d2a139a5c3954e0e0243d6961c17d4515
ReemAbdulraouf <76842378+ReemAbdulraouf@users.noreply.github.com>
2024-04-27 16:22:00 +0300

    update in sql files

--- dags/Reem/Sql/populate_dim_customer.sql
+++ dags/Reem/Sql/populate_dim_customer.sql
@@ -2,7 +2,7 @@ INSERT INTO Data_Platform_Reema.dim_customer
 (customer_id, customer_name, email, phone , address_id, address_street, address_zipcode, city_id, city_name
 , state_id, state_name, country_id, country_name)
 
-select c.customer_id , c."name" customer_name, c.email , c.phone
+select c.customer_id , c.name customer_name, c.email , c.phone
 , a.address_id , a.street address_street, a.zipcode address_zipcode
 , -1 city_id, a.city city_name
 , -1 state_id, a.state state_name
