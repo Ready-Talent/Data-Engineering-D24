b11bdf9a842a894e2f6aa869b3c199c9c4a28237
Mohamed Ashraf <mohamed.ashraf211087@gmail.com>
2024-04-27 05:54:35 -0700

    populate_dim_tabels

---
+++ dags/Ashraf/sql/dim_date.sql
@@ -0,0 +1,16 @@
+INSERT INTO data_platform_01.dim_date
+(date_key,date, day_of_week, day_name, day_of_month, day_of_year, week_of_year, month_name, quarter, year)
+
+SELECT
+        CAST(FORMAT_DATE('%Y%m%d', DATE_ADD(DATE '2024-01-01', INTERVAL n DAY)) AS INT64) AS date_key,
+        DATE_ADD(DATE '2024-01-01', INTERVAL n DAY) AS date,
+        EXTRACT(YEAR FROM DATE_ADD(DATE '2024-01-01', INTERVAL n DAY)) AS year,
+        EXTRACT(MONTH FROM DATE_ADD(DATE '2024-01-01', INTERVAL n DAY)) AS month_name,
+        EXTRACT(DAY FROM DATE_ADD(DATE '2024-01-01', INTERVAL n DAY)) AS day_of_month,
+        EXTRACT(QUARTER FROM DATE_ADD(DATE '2024-01-01', INTERVAL n DAY)) AS quarter,
+        EXTRACT(DAYOFWEEK FROM DATE_ADD(DATE '2024-01-01', INTERVAL n DAY)) AS day_of_week,
+        FORMAT_DATE('%A', DATE_ADD(DATE '2024-01-01', INTERVAL n DAY)) AS day_name,
+        EXTRACT(WEEK FROM DATE_ADD(DATE '2024-01-01', INTERVAL n DAY)) AS week_of_year,
+        EXTRACT(DAYOFYEAR FROM DATE_ADD(DATE '2024-01-01', INTERVAL n DAY)) AS day_of_year,
+    FROM 
+        UNNEST(GENERATE_ARRAY(0, DATE_DIFF(DATE '2025-01-01', DATE '2024-01-01', DAY))) AS n
---
+++ dags/Ashraf/sql/dim_product.sql
@@ -0,0 +1,10 @@
+INSERT INTO data_platform_01.dim_product
+(product_id, brand_id, category_id, name, price, description, brand_name, category_name)
+
+select p.product_id , -1 brand_id, -1 category_id
+, substring(name from '\s(.*)') AS product_name
+,p.price
+,p.description
+, split_part(p.name, ' ', 1) AS brand_name
+, 'Cell Phones' category_name
+from replica_01.product p;
