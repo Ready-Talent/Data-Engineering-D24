a05376717c0c1054af28558e723c19f5e3859b07
omar-gamal99 <omar.thabet@talabat.com>
2024-04-27 15:02:27 +0300

    create dim_customer

---
+++ dags/omar_thabet/create_dim_customer.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.providers.google.cloud.transfers.postgres_to_gcs import (
+    BigQueryExecuteQueryOperator,
+)
+
+dag = DAG(
+    dag_id="dim_customer_creation",
+    description="create dim_customer",
+    schedule_interval=None,
+    start_date=datetime(2024, 4, 20),
+    catchup=False,
+)
+
+start = EmptyOperator(task_id="start", dag=dag)
+
+end = EmptyOperator(task_id="end", dag=dag)
+
+create_table_ = BigQueryExecuteQueryOperator(
+    task_id="create_table",
+    sql="sql/create_dim_customer.sql",
+    use_legacy_sql=False,
+    dag=dag,
+)
+
+populate_table = BigQueryExecuteQueryOperator(
+    task_id="fill_table", sql="sql/dim_customer.sql", use_legacy_sql=False, dag=dag
+)
+
+start >> create_table_ >> populate_table >> end
