d802c858b26f2324e222b7fb7c2caba6c5c63ffc
lojainwail01 <lojainph@gmail.com>
2024-04-27 16:37:15 +0300

    lojain create dim v.5

--- dags/lojain/create_tables.sql
+++ dags/lojain/create_tables.sql
@@ -36,16 +36,7 @@ CREATE TABLE IF NOT EXISTS Data_Platform_Lojain.dim_product (
     modified_by STRING,
     modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP()
 );
-INSERT INTO Data_Platform_Lojain.junk_dim (payment_type_code, payment_type_name, channel_code, channel_name)
-WITH payment_types AS (
-    SELECT CAST(pt.payment_type_id AS INT64) AS payment_type_code, pt.name AS payment_type_name 
-    FROM landing.payment_type pt
-    UNION ALL
-    SELECT -1 AS payment_type_code, 'N/A' AS payment_type_name
-)
-SELECT pt.payment_type_code, pt.payment_type_name, c.channel_id AS channel_code, c.name AS channel_name
-FROM landing.channel c
-CROSS JOIN payment_types pt;
+
 
 CREATE TABLE IF NOT EXISTS Data_Platform_Lojain.dim_date (
     date_key INT64,
@@ -69,4 +60,15 @@ CREATE TABLE IF NOT EXISTS Data_Platform_Lojain.dim_time (
     full_time TIME,
     hour INT64,
     minute INT64
+);
+CREATE TABLE IF NOT EXISTS PL.junk_dim (
+    junk_key INT64,
+    payment_type_code INT64,
+    payment_type_name STRING,
+    channel_code INT64,
+    channel_name STRING,
+    created_by STRING DEFAULT 'user',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+    modified_by STRING,
+    modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP()
 );
