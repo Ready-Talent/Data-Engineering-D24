dcdb7f86f9c2d109a8be52ad7d7382091ee7530d
rowym <rowymm.ammar@gmail.com>
2024-04-27 16:18:44 +0300

    rrrrr

--- dags/rowym_branch/SQL/sqlQueries_create.sql
+++ dags/rowym_branch/SQL/sqlQueries_create.sql
@@ -9,17 +9,3 @@ CREATE TABLE IF NOT EXISTS Data_Platform_Rowym.dim_customer (
 	modified_by STRING,
 	modified_at TIMESTAMP
 );
-
-INSERT INTO Data_Platform_Rowym.dim_customer (customer_id, customer_name, address_id, address_street, zipcode, created_by, created_at, modified_by, modified_at)
-select 
-	customer.customer_id as customer_id,
-	customer.name as customer_name,
-	address.address_id as address_id,
-	address.street as address_street,
-	address.zipcode as address_zipcode,
-	customer.created_by as created_by,
-	customer.created_at as created_at,
-	customer.modified_by as modified_by,
-	customer.modified_at as modified_at 
-from SRC_05.customer  
-left join SRC_05.address on address.customer_id = customer.customer_id ;
