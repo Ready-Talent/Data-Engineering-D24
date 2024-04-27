031c2dab6697ced3b3068de5d78cfbe3379d683b
omar-gamal99 <omar.thabet@talabat.com>
2024-04-27 12:24:57 +0300

    fix sql

--- dags/omar_thabet/sql/create_dim_customer.sql
+++ dags/omar_thabet/sql/create_dim_customer.sql
@@ -3,7 +3,7 @@ CREATE TABLE  data_platform.dim_customer (
     customer_id INT64,
     customer_name STRING,
     email STRING,
-    phone STRING,
+    phone INT64,
     address_id INT64,
     address_street STRING,
     address_zipcode STRING,
