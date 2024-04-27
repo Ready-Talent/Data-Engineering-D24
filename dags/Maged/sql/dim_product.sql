a1e8285dd824bcdd67defd2b241c33b59e06f5dd
AhmadMagedd20 <ahmadmagedd5@gmail.com>
2024-04-27 16:17:38 +0300

    fix

--- dags/Maged/sql/dim_product.sql
+++ dags/Maged/sql/dim_product.sql
@@ -1,10 +1,10 @@
 INSERT INTO Data_Platform_Maged.dim_product
-(product_id, brand_id, category_id, name, price, description, brand_name, category_name)
+(product_id, brand_id, category_id, product_name, price, description, brand_name, category_name)
 
 select p.product_id , -1 brand_id, -1 category_id
-, substring(name from '\s(.*)') AS product_name
+,p.name As product_name
 ,p.price
 ,p.description
-, split_part(p.name, ' ', 1) AS brand_name
+, split(p.name, ' ')[0] AS brand_name
 , 'Cell Phones' category_name
 from landing.product p;
