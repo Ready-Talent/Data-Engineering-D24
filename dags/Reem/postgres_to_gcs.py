252b74aab0af1f03dd000602dda178e23124519c
ReemAbdulraouf <76842378+ReemAbdulraouf@users.noreply.github.com>
2024-04-28 10:59:25 +0300

    Updatess

--- dags/Reem/postgres_to_gcs.py
+++ dags/Reem/postgres_to_gcs.py
@@ -1,10 +1,9 @@
 import logging
-from datetime import datetime
-
 from airflow import DAG
+from datetime import datetime
 from airflow.operators.empty import EmptyOperator
-from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
 from airflow.providers.google.cloud.transfers.postgres_to_gcs import PostgresToGCSOperator
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
 
 
 
@@ -19,37 +18,34 @@ dag = DAG(
 start_task = EmptyOperator(task_id="start_task", dag=dag)
 end_task = EmptyOperator(task_id="end_task", dag=dag)
 
-GCS_BUCKET = "postgres-to-gcs"
 
-tables = ["product", "customer", "address", "order_detail", "payment", "payment_type", "channel"]
+tables = ['product','order','order_detail','channel','payment','payment_type','address','customer',]
 
 
 for table in tables:
-
-    get_data = PostgresToGCSOperator(
-        task_id=f"postgres_to_gcs{table}",
-        postgres_conn_id="postgres_conn_reema",
-        sql=f"SELECT * from src01.{table}",
-        bucket=GCS_BUCKET,
-        export_format="CSV",
-        filename="Reemaa/order.csv",
-        gzip=False,
-        dag=dag
+    postgres_to_gcs = PostgresToGCSOperator(
+    task_id=f"postgres_to_gcs{table}",
+    postgres_conn_id="postgres_conn_reema",
+    sql=f"SELECT * FROM src01.{table}",
+    bucket="postgres-to-gcs",
+    export_format = "CSV",
+    filename=f"Reemaa/{table}.csv",
+    gzip=False,
+    dag = dag
     )
 
-
-    load_csv = GCSToBigQueryOperator(
-        task_id=f"gcs_to_bigquery{table}",
-        bucket=GCS_BUCKET,
-        source_objects=[f"Reemaa/{table}.csv"],
-        destination_project_dataset_table=f"Reema_AirFlow.{table}",
-        create_disposition='CREATE_IF_NEEDED',
-        write_disposition='WRITE_TRUNCATE',
-        field_delimiter=',',
-        skip_leading_rows=1,
-        ignore_unknown_values=True,
-        max_bad_records= 1000000,
-        dag=dag
-    )
-
-    start_task >> get_data >> load_csv >>end_task
+    gcs_to_bigquery = GCSToBigQueryOperator(
+    task_id=f"gcs_to_bigquery{table}",
+    bucket="postgres-to-gcs",
+    source_objects=[f"Reemaa/{table}.csv"],
+    destination_project_dataset_table= f"Reema_AirFlow.{table}",
+    field_delimiter=',',
+    max_bad_records = 1000000,
+    skip_leading_rows = 1,
+    ignore_unknown_values = True,
+    source_format = "CSV",
+    autodetect = True,
+    write_disposition = "WRITE_TRUNCATE",
+    dag = dag
+    ) 
+    start_task >> postgres_to_gcs >> gcs_to_bigquery >> end_task
