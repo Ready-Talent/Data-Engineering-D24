7ea00148dfeda0a8209e57580eab8d4c1795e5f3
Abduallah <Abduallahhussien11@gmail.com>
2024-05-07 01:38:15 +0300

    jj

--- dags/abduallah_hussien/dim_date.sql
+++ dags/abduallah_hussien/dim_date.sql
@@ -10,12 +10,12 @@ create table if not exists `ready-data-engineering-p24.Data_Platform_Abduallah.d
     month_of_year INT64,
     quarter INT64,
     year INT64,
-    holiday_flag STRING,
-    weekend_flag STRING,
+    --holiday_flag STRING,
+    --weekend_flag STRING,
     record_updated_date timestamp default CURRENT_TIMESTAMP()
 );
- INSERT INTO `ready-data-engineering-p24.Data_Platform_Abduallah.dim_date` (date_key,date,day_of_week,day_name,day_of_month,day_of_year,week_of_year,month_name,month_of_year,quarter,year,holiday_flag,weekend_flag)
+ INSERT INTO `ready-data-engineering-p24.Data_Platform_Abduallah.dim_date` (date_key,date,day_of_week,day_name,day_of_month,day_of_year,week_of_year,month_name,month_of_year,quarter,year)
              SELECT 
-            date_key,date,day_of_week,day_name,day_of_month,day_of_year,week_of_year,month_name,month_of_year,quarter,year,holiday_flag,weekend_flag
+            date_key,date,day_of_week,day_name,day_of_month,day_of_year,week_of_year,month_name,month_of_year,quarter,year
             FROM
                 `ready-data-engineering-p24.data_platform.dim_date`
