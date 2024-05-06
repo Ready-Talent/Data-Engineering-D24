fa59119c46d09c5be8af82a994eaf891bcbdd4bc
Abduallah <Abduallahhussien11@gmail.com>
2024-05-06 16:04:06 +0300

    hh

--- dags/abduallah_hussien/dim_date.sql
+++ dags/abduallah_hussien/dim_date.sql
@@ -1,4 +1,3 @@
--- create dim_date
 create table if not exists `ready-data-engineering-p24.Data_Platform_Abduallah.dim_date` (
     date_key INT64,
     date date,
@@ -14,19 +13,4 @@ create table if not exists `ready-data-engineering-p24.Data_Platform_Abduallah.d
     holiday_flag STRING,
     weekend_flag STRING,
     record_updated_date timestamp default CURRENT_TIMESTAMP()
-);
-CREATE TABLE IF NOT EXISTS `ready-data-engineering-p24.Data_Platform_Abduallah.dim_product` (
-  product_key INT64,
-  product_id INT64,
-  brand_id INT64,
-  category_id INT64,
-  name STRING,
-  price NUMERIC(10, 2),
-  description STRING,
-  brand_name STRING,
-  category_name STRING,
-  created_by STRING DEFAULT 'user',
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
-  modified_by STRING,
-  modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP()
 )
