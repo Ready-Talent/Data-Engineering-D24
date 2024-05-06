b7968769a4124f3c7436979c53477bbe2978b8fa
Abduallah <Abduallahhussien11@gmail.com>
2024-05-06 15:57:41 +0300

    change sql files

--- dags/abduallah_hussien/dim_date.sql
+++ dags/abduallah_hussien/dim_date.sql
@@ -1,17 +1,32 @@
 -- create dim_date
 create table if not exists `ready-data-engineering-p24.Data_Platform_Abduallah.dim_date` (
-    date_key serial primary key,
+    date_key INT64,
     date date,
-    day_of_week int,
-    day_name varchar(10),
-    day_of_month int,
-    day_of_year int,
-    week_of_year int,
-    month_name varchar(10),
-    month_of_year int,
-    quarter int,
-    year int,
-    holiday_flag boolean,
-    weekend_flag boolean,
-    record_updated_date timestamp default now()
+    day_of_week INT64,
+    day_name STRING(10),
+    day_of_month INT64,
+    day_of_year INT64,
+    week_of_year INT64,
+    month_name STRING(10),
+    month_of_year INT64,
+    quarter INT64,
+    year INT64,
+    holiday_flag STRING,
+    weekend_flag STRING,
+    record_updated_date timestamp default CURRENT_TIMESTAMP()
 );
+CREATE TABLE IF NOT EXISTS `ready-data-engineering-p24.Data_Platform_Abduallah.dim_product` (
+  product_key INT64,
+  product_id INT64,
+  brand_id INT64,
+  category_id INT64,
+  name STRING,
+  price NUMERIC(10, 2),
+  description STRING,
+  brand_name STRING,
+  category_name STRING,
+  created_by STRING DEFAULT 'user',
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+  modified_by STRING,
+  modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP()
+)
