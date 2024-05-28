7f059d7dffe67241dbbcc041e14b506fda187467
NadineNoureldin <nadinenoureldin24@gmail.com>
2024-05-28 17:14:51 +0000

    create dim_location

---
+++ dbt/nadine/models/example/dim_location.sql
@@ -0,0 +1,10 @@
+select 
+    dropoff_longitude,
+    dropoff_latitude,
+    dropoff_location,
+    pickup_longitude,
+    pickup_latitude,
+    pickup_location,
+    pickup_community_area
+
+from ready-data-engineering-p24.Nadine_Airflow.chicago-taxi
