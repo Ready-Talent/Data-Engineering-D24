167f0bfd068fcf0dafc0fa562b605e55b87d972a
abduallahhussienn@gmail.com <abduallahhussienn@gmail.com>
2024-05-28 18:38:41 +0000

    last_test

--- dbt/omar_thabet/models/modeling/fct_trip.sql
+++ dbt/omar_thabet/models/modeling/fct_trip.sql
@@ -1,8 +1,7 @@
 WITH fct_trip AS (
     SELECT
         unique_key,
-        taxi_id,
-        company,
+        tr.taxi_id,
         trip_start_timestamp,
         trip_end_timestamp,
         trip_seconds,
@@ -12,24 +11,35 @@ WITH fct_trip AS (
         tolls,
         extras,
         trip_total,
-        payment_type,
-        pickup_location,
-        pickup_latitude,
-        pickup_longitude,
-        pickup_census_tract,
-        pickup_community_area,
-        pickup_location
-        dropoff_location,
-        dropoff_latitude,
-        dropoff_longitude,
-        dropoff_census_tract,
-        dropoff_community_area,
+        pt.payment_type_id,
+        pl.location_id as pickup_location_id,
+        dl.location_id as dropoff_location_id,
         dim_date.date AS trip_date,
         dim_date.day_name AS trip_day_name,
         dim_date.day_of_week AS day_of_week,
         dim_date.day_of_month AS day_of_month,
     FROM 
-        `ready-data-engineering-p24.chicago_taxi_OT.chicago-taxi-test-de24_OT` trips
+        `ready-data-engineering-p24.chicago_taxi_OT.chicago-taxi-test-de24_OT` tr
+    left join 
+        {{ ref('dim_location') }} pl on
+        pickup_location = location
+        and pickup_latitude = latitude
+        and pickup_longitude = longitude
+        and pickup_census_tract = census_tract
+        and pickup_community_area = community_area
+    left join 
+        {{ ref('dim_location') }} dl on
+        dropoff_location = location
+        and dropoff_latitude = latitude
+        and dropoff_longitude = longitude
+        and dropoff_census_tract = census_tract
+        and dropoff_community_area = community_area
+    left join 
+         {{ ref('dim_payment_type') }} pt on
+         pt.payment_type = tr.payment_type
+    left join
+         {{ ref('dim_taxi') }} t on
+         t.taxi_id = tr.taxi_id
     LEFT JOIN
         {{ ref('dim_date') }}  ON CAST(FORMAT_DATE('%Y%m%d', DATE(trips.trip_end_timestamp)) AS INT64) = dim_date.date_id
 )
