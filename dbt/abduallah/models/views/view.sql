de11ca40ee803d804a6140e04a30b84a64c2d83f
abduallahhussienn@gmail.com <abduallahhussienn@gmail.com>
2024-06-01 12:51:10 +0000

    m view 3

--- dbt/abduallah/models/views/view.sql
+++ dbt/abduallah/models/views/view.sql
@@ -2,7 +2,7 @@ select taxi_id,
 tips,
 trip_total,
 trip_miles,
-timestamp_diff(MINUTE,trip_end_timestamp,trip_start_timestamp),
+timestamp_diff(trip_end_timestamp,trip_start_timestamp),
 payment_typ_id
 FROM
 {{ ref('fact_trips') }}
